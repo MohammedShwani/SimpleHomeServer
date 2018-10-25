2b69aa17037d1a3459f07e209a50905a4c8a3c91
Mohammed Yousif <humana.human@gmail.com>
2018-10-25 17:05:40 +0300

    Minor improvements

--- server.py
+++ server.py
@@ -1,31 +1,31 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from load_configrations import Config
-
-#load configration from .env file
-try:
-    config = Config()
-    config.load()
-except Exception as msg:
-    print("-- {msg}".format(msg=msg))
-
-print(config.get('SERV_ROOT_PATH'))
+from configrations import Config
 
 #create server class
-class Serv(BaseHTTPRequestHandler):
-
+class SHServer(BaseHTTPRequestHandler):
+    global config
     def do_GET(self):
         if self.path == '/':
             self.path = '/index.html'
         try:
-            file_to_open = open(config.get('SERV_ROOT_PATH') + self.path[1:]).read()
+            fileToOpen = open(config.get('SERV_ROOT_PATH') + self.path[1:]).read()
             self.send_response(200)
         except:
-            file_to_open = "File not found"
+            fileToOpen = "File not found"
             self.send_response(404)
         self.end_headers()
-        self.wfile.write(bytes(file_to_open, 'utf-8'))
+        self.wfile.write(bytes(fileToOpen, 'utf-8'))
 
 
-#start server
-httpd = HTTPServer((config.get('SERV_NAME'), int(config.get('SERV_PORT'))), Serv)
-httpd.serve_forever()
+try:
+    #load configration from .env file
+    config = Config()
+    config.load()
+
+    print(config.get('SERV_ROOT_PATH'))
+
+    httpd = HTTPServer((config.get('SERV_NAME'), int(config.get('SERV_PORT'))), SHServer)
+    httpd.serve_forever()
+
+except Exception as msg:
+    print("-- {msg}".format(msg=msg))
